33fe25e0c3325d0673a5a38e981dda207315853a
Lorenzo <baccini.lorenzo@gmail.com>
2021-11-05 00:53:28 +0100

    Un po' di scss

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,9 +1,18 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <ul>
-        @foreach ($posts as $post)
-            <li><a href="{{ route("admin.posts.show", $post["slug"]) }}">{{ $post["title"] }}</a></li>
-        @endforeach
-    </ul>
+    <div class="container_posts_admin">
+
+        <ul>
+            @foreach ($posts as $post)
+            <a href="{{ route("admin.posts.show", $post["slug"]) }}">
+                <li>
+                    <h2>{{ $post["title"] }}</h2>
+                    <p>{{ $post["content"] }}</p>
+                </li>
+            </a>
+            @endforeach
+        </ul>
+
+    </div>
 @endsection
